00438c4ec9d32bbe3936e058e7c72a339f2f4576
bhavdeep reddy <87595670+Bhavadee@users.noreply.github.com>
2025-04-22 16:59:48 +0000

    commit

---
+++ datatypes/String.py
@@ -0,0 +1,14 @@
+str1 = "Hello"
+str2 = "World"
+result = str1 + " " + str2
+print(result)
+
+text = "Python is awesome"
+length = len(text)
+print("Length of the string:", length)
+
+text = "Python is awesome"
+uppercase = text.upper()
+lowercase = text.lower()
+print("Uppercase:", uppercase)
+print("Lowercase:", lowercase)
---
+++ vairables/gobal.py
@@ -0,0 +1,7 @@
+y = 20 
+
+def another_function():
+    print(y)  
+
+another_function()
+print(y)  
